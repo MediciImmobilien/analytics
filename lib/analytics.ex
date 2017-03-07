931ca7e1cacfa3df2fd6ad06e4fe21ea19d9614f
MediciImmobilien <lutzvogelsang@Lutzs-MacBook-Pro-6.local>
2017-03-07 14:41:55 +0100

    fix

--- lib/analytics.ex
+++ lib/analytics.ex
@@ -20,17 +20,17 @@ defmodule Analytics do
 	end	
 	
 	def get_data(:impression, :last_month, :list) do
-		{:ok, data} = request_data([current_month()], ["ga:impressions"],["ga:day"])
+		{:ok, data} = request_data([last_month()], ["ga:impressions"],["ga:day"])
 		data
 	end
 	
 	def get_data(:adcost, :last_month, :list) do
-		{:ok, data} = request_data([current_month()], ["ga:adCost"],["ga:day"])
+		{:ok, data} = request_data([last_month()], ["ga:adCost"],["ga:day"])
 		data
 	end
 	
 	def get_data(:ad_cost, :last_month, :sum) do
-		{:ok, data} =  request_data([current_month()], ["ga:adCost"],["ga:day"]) 
+		{:ok, data} =  request_data([last_month()], ["ga:adCost"],["ga:day"]) 
 		data
 	 	|> Enum.map(fn(%{value: value}) -> value end)
 		|> Enum.sum
