4454b83977068998f958a0d62e0c14cd90e1b4c8
MediciImmobilien <lutzvogelsang@Lutzs-MacBook-Pro-6.local>
2017-05-22 19:58:29 +0200

    fix

--- lib/analytics.ex
+++ lib/analytics.ex
@@ -24,7 +24,7 @@ defmodule Analytics do
 		{:ok, data} = request_data([current_year()], ["ga:adCost"],["ga:month"])
 		data
 		|> Enum.map(fn(%{"ga:adCost" => cost, "ga:month" => month}) -> cost |> Currency.from_usd_to_eur  |> IO.inspect end)
-		data		
+			
 	end
 	
 	def get_data(:ad_cost, :current_month, :sum) do
