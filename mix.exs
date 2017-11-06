b970e9b709f1313de43a3a12189f4c24df0f1c04
MediciImmobilien <lutzvogelsang@Lutzs-MacBook-Pro-6.local>
2017-11-06 17:34:50 +0100

    fix

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Analytics.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [ 	{:timex, "~> 3.1"}
+    [ 	{:timex, "~> 3.1"},
 		{:poison, "~> 3.1",override: :true},
 		{:httpoison, "~> 0.11.0"},
 		{:money, "~> 1.2"},
